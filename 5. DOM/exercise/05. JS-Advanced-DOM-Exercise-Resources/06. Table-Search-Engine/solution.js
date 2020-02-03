8e7809abaa3a90b9c70c3e67ac5443bfff0991dd
nikolaynikolo91 <57035504+nikolaynikolo91@users.noreply.github.com>
2020-02-03 02:45:51 +0200

    initial commit

--- 5. DOM/exercise/05. JS-Advanced-DOM-Exercise-Resources/06. Table-Search-Engine/solution.js
+++ 5. DOM/exercise/05. JS-Advanced-DOM-Exercise-Resources/06. Table-Search-Engine/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-   //TODO...
+  let allRows = Array.from(document.querySelectorAll("tbody  tr"));
+  let button = document.querySelector("button");
+  let input = document.querySelector("input");
+
+  button.addEventListener("click", () => {
+    let regex = new RegExp(input.value, "gim");
+
+    allRows.map(e => {
+      e.classList.remove("select");
+
+      if (e.innerHTML.match(regex) !== null) {
+        e.classList.add("select");
+      }
+    });
+    input.value = "";
+  });
 }
