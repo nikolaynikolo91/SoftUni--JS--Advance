00b44a280cead211b17599754f7e548d4d144563
nikolaynikolo91 <57035504+nikolaynikolo91@users.noreply.github.com>
2020-11-01 00:29:33 +0200

    chenge folder name

--- 1.0. Syntax-Functions-and-Statements-Lab/.history/09. Words Uppercase_20200928172500.js
+++
@@ -1,9 +0,0 @@
-function solve (input) {
-///let regex = /([a-zA-Z]+)/g
-//let matched = input.match(regex);
-//console.log(matched.map(a => a.toUpperCase()).join(', '))
-console.log(input.toUpperCase())
-}
-
-solve ('Hi, how are you?')
-solve ('hello')
--- 1.0. Syntax-Functions-and-Statements-Lab/.history/09. Words Uppercase_20200928172915.js
+++
@@ -1,9 +0,0 @@
-function solve(input) {
-    let regex = /([\w]+)/g
-    let matched = input.match(regex);
-    console.log(matched.map(a => a.toUpperCase()).join(', '))
-
-}
-
-solve('Hi, how are you?')
-solve('hello')
