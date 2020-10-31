31ed95fbb9e91447ef77e098bf72bbeb4b412ae6
nikolaynikolo91 <57035504+nikolaynikolo91@users.noreply.github.com>
2020-11-01 00:30:57 +0200

    remove .history folder

--- 1.1. Syntax-Functions-and-Statements-Exercise/.history/01. Fruit_20200928173328.js
+++
@@ -1,8 +0,0 @@
-function solve (fruit, weight, perKg){
-let sum = (weight/1000) * perKg;
-console.log(`I need $${sum.toFixed(2)} to buy ${(weight/1000).toFixed(2)} kilograms ${fruit}.`)
-}
-
-
-
-solve ('orange', 2500, 1.80)
